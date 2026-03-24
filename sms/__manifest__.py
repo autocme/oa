c24466b312df033f5b03265d05313668ecbe7e6b
autocme <autocme20@gmail.com>
2026-03-24 14:27:03 +0300

    Update __manifest__.py

--- sms/__manifest__.py
+++ sms/__manifest__.py
@@ -42,7 +42,7 @@ The service is provided by the In App Purchase Odoo platform.
         'data/mail_demo.xml',
     ],
     'installable': True,
-    'auto_install': True,
+
     'assets': {
         'web.assets_backend': [
             'sms/static/src/**/*',
