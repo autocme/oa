59d5fb4c81d4fdd869fdf645fd7440b4b46af66b
autocme <autocme20@gmail.com>
2026-07-10 11:22:56 +0300

    Update installable status and auto_install setting
    Set installable to False and removed auto_install.

--- spreadsheet_dashboard_website_sale_slides/__manifest__.py
+++ spreadsheet_dashboard_website_sale_slides/__manifest__.py
@@ -9,8 +9,7 @@
     'data': [
         "data/dashboards.xml",
     ],
-    'installable': True,
-    'auto_install': ['website_sale_slides'],
+    'installable': False,
     'author': 'Odoo S.A.',
     'license': 'LGPL-3',
 }
