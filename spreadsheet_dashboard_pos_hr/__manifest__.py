02a790d1a6a477235f8eb9bee3bc488747713f3b
autocme <autocme20@gmail.com>
2026-07-10 11:20:33 +0300

    Update __manifest__.py

--- spreadsheet_dashboard_pos_hr/__manifest__.py
+++ spreadsheet_dashboard_pos_hr/__manifest__.py
@@ -10,7 +10,7 @@
     'data': [
         "data/dashboards.xml",
     ],
-    'installable': True,
+    'installable': False,
     'auto_install': ['pos_hr'],
     'author': 'Odoo S.A.',
     'license': 'LGPL-3',
